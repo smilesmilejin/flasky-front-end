59fd893977933e6ebf12ea6c90e9b32c912ff8e7
Ashley Yang <ashley.minghui@gmail.com>
2025-06-17 11:05:50 -0700

    add useEffect hook to get all cats after render from backend API, add fn to pet a cat and send a request to the backend to update the cat record in the DB, add fn to delete cat from the db

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
+import axios from 'axios';
 import CatList from './components/CatList.jsx';
-import DATA from './data.json';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const kBaseUrl = 'http://localhost:5000';
+
+const getAllCatsApi = () => {
+  return axios.get(`${kBaseUrl}/cats`)
+    .then( response => {
+      return response.data.map(convertFromApi);
+    })
+    .catch( error => {
+      console.log(error);
+    });
+};
+
+const convertFromApi = (apiCat) => {
+  // destructure cat and create variables
+  // use variables to create a new cat object that has petCount as the name of the attribute
+  const { id, name, color, personality, pet_count, caretaker } = apiCat;
+  const newCat = {id, name, color, personality, petCount: pet_count, caretaker};
+  return newCat;
+};
+
+const petCatApi = (id) => {
+  return axios.patch(`${kBaseUrl}/cats/${id}/pet`)
+    .then(response => {
+      return convertFromApi(response.data);
+    })
+  .catch(error => {
+    console.log(error);
+  });
+};
+
+const removeCatApi = (id) => {
+  return axios.delete(`${kBaseUrl}/cats/${id}`)
+    .catch(error => {
+      console.log(error);
+    });
+};
 
 const calculateTotalPets = (catData) => {
   // let totalPets = 0;
@@ -15,8 +52,17 @@ const calculateTotalPets = (catData) => {
 };
 
 function App() {
-  const [catData, setCatData] = useState(DATA);
+  const [catData, setCatData] = useState([]);
   const totalPets = calculateTotalPets(catData);
+
+  const getAllCats = () => {
+    return getAllCatsApi()
+      .then(cats => setCatData(cats));
+  };
+
+  useEffect(() => {
+    getAllCats();
+  }, []);
 
   const petCat = (id) => {
     // Call the set state of the data
@@ -25,23 +71,35 @@ function App() {
         // copy the existing array, looking for the matching cat id
         // if the id matches, create a new record with updated count
         // if not, reuse the existing record
-
-    setCatData(catData => {
-      return catData.map(cat => {
-        if (cat.id === id) {
-          return {...cat, petCount: cat.petCount + 1};
-        } else {
-          return cat;
-        }
+    return petCatApi(id)
+      .then(catResult => {
+        setCatData(catData => catData.map(cat => {
+          if (cat.id === catResult.id) {
+            return catResult;
+          } else {
+            return cat;
+          }
+        }));
       });
-    });
+    
+    // updating state after petting a cat with hardcoded data
+    // setCatData(catData => {
+    //   return catData.map(cat => {
+    //     if (cat.id === id) {
+    //       return {...cat, petCount: cat.petCount + 1};
+    //     } else {
+    //       return cat;
+    //     }
+    //   });
+    // });
   };
 
-  const removeCat = (id) => {
-    setCatData(catData => {
-      return catData.filter(cat => {
+  const removeCat = id => {
+    return removeCatApi(id)
+    .then(() => {
+      setCatData(catData => catData.filter(cat => {
         return cat.id !== id;
-      });
+      }));
     });
   };
 
