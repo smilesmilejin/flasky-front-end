e84155314ca664db7edc03d367ea064b8db70c7e
Ansel Rognlie <ansel@adadevelopersacademy.org>
2025-06-20 10:48:13 -0700

    More Forms roundtable commit

--- src/components/NewCatForm.jsx
+++ src/components/NewCatForm.jsx
@@ -1,37 +1,56 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
+const kDefaultFormState = {
+  name: '',
+  personality: '',
+  color: '',
+};
+
 const NewCatForm = ({ onPostCat }) => {
-  const [name, setName] = useState('');
+  const [formData, setFormData] = useState(kDefaultFormState);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    const newCat = {
-      name,
-      personality: '',
-      color: '',
-    };
-
-    onPostCat(newCat);
-    setName('');
+    onPostCat(formData);
+    setFormData(kDefaultFormState);
   };
 
   const handleChange = (event) => {
-    setName(event.target.value);
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+
+    setFormData(formData => {
+      return { ...formData, [inputName]: inputValue };
+    });
+  };
+
+  const makeControlledInput = (inputName) => {
+    return <input 
+      onChange={handleChange} 
+      type='text' 
+      id={`input-${inputName}`}
+      name={inputName}
+      value={formData[inputName]}
+      />;
   };
 
   return (
     <form onSubmit={handleSubmit}>
         <div>
+          <div>
             <label htmlFor='input-name'>Cat Name: </label>
-            <input 
-            onChange={handleChange} 
-            type='text' 
-            id='input-name' 
-            name='name' 
-            value={name}
-            />
+            { makeControlledInput('name') }
+            </div>
+            <div>
+            <label htmlFor='input-personality'>Personality: </label>
+            { makeControlledInput('personality') }
+            </div>
+            <div>
+            <label htmlFor='input-color'>Color: </label>
+            { makeControlledInput('color') }
+            </div>
         </div>
         <div>
             <button className='form-button'>Add Cat</button>
